6259b428f477e54e5bfe108297605af5c8f2c230
EC2 Default User <ec2-user@ip-172-31-11-143.ec2.internal>
2022-05-06 14:57:26 +0000

    fix favorite model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
+  root to: "homes#top"
+  get "/home/about" => "homes#about", as: "about"
 
   resources :books, only: [:create, :show, :index, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
   resources :users, only: [:index, :show, :edit, :update]
 
-  get "/home/about" => "homes#about", as: "about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
